fdfc1ca0d21855666f2162d867e2ad93ae3248af
yangbin <18774625353@139.com>
2015-04-29 11:51:08 +0800

    Initial Commit

---
+++ YBAttributedLabelTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YBAttributedLabelTest
+//
+//  Created by 杨彬 on 15/4/29.
+//  Copyright (c) 2015年 macbook air. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
